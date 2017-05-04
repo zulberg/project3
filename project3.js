271a946433378c4925a862eef8d423f88a102d7f
Unknown <zulberg@mail.csuchico.edu>
2017-05-03 17:16:34 -0700

    More complete...

--- project3.js
+++ project3.js
@@ -1,7 +1,38 @@
+var score=document.getElementById("fico").value;
+var autopmt[];
+var homepmt[];
+var autoInterestRate=[.225, .175, .125, .089, .049, .009];
+var homeInterestRate=[.1375, .1085, .0675, .0475, .0395, .0325]
+
 function nextPage() {
 window.location.href="page1.html";
 }
 
-function ficoscore() {
-  
+function ficoscore(score, autoInterestRate, homeInterestRate) {
+  if (score < 300) {
+    autoInterestRate = .225;
+    homeInterestRate = .1375;
+  } else if (score >= 300 && score <= 629) {
+    autoInterestRate = .175;
+    homeInterestRate = .1085;
+  } else if (score >= 630 && score <= 689) {
+    autoInterestRate = .125;
+    homeInterestRate = .0675;
+  } else if (score >= 690 && score <= 719) {
+    autoInterestRate = .089;
+    homeInterestRate = .0475;
+  } else if (score >= 720 && score <= 789) {
+    autoInterestRate = .049;
+    homeInterestRate = .0395;
+  } else if (score >= 790) {
+    autoInterestRate = .009;
+    homeInterestRate = .0325;
+  }
+}
+
+function autoLoanAmt(score) {
+
+  if (score < 300) {
+
+  }
 }
