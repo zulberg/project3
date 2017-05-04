cd08017d5d47362cd116832688766c14a3681a43
Unknown <zulberg@mail.csuchico.edu>
2017-05-04 12:48:29 -0700

    More progress

--- project3.js
+++ project3.js
@@ -1,5 +1,5 @@
 var score=document.getElementById("fico").value;
-var autoprice=document.getElementById("price").value;
+var autoprice=document.getElementById("aprice").value;
 var homeprice=document.getElementById("hprice").value;
 var autoDownPayment=document.getElementById("autodownpmt").value;
 var homeDownPayment=document.getElementById("homedownpmt").value;
@@ -7,36 +7,67 @@ var autopmt[];
 var homepmt[];
 var autoInterestRate=[.225, .175, .125, .089, .049, .009];
 var homeInterestRate=[.1375, .1085, .0675, .0475, .0395, .0325]
+var autorate;
+var homerate;
 
 function nextPage() {
 window.location.href="page1.html";
 }
 
-function ficoscore(score, autoInterestRate, homeInterestRate) {
+function autoficoscore(score, autoInterestRate) {
+  var index;
   if (score < 300) {
-    autoInterestRate = .225;
-    homeInterestRate = .1375;
+    index=0;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $5,000.";
   } else if (score >= 300 && score <= 629) {
-    autoInterestRate = .175;
-    homeInterestRate = .1085;
+    index=1;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $10,000.";
   } else if (score >= 630 && score <= 689) {
-    autoInterestRate = .125;
-    homeInterestRate = .0675;
+    index=2;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $16,500.";
   } else if (score >= 690 && score <= 719) {
-    autoInterestRate = .089;
-    homeInterestRate = .0475;
+    index=3;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $22,500.";
   } else if (score >= 720 && score <= 789) {
-    autoInterestRate = .049;
-    homeInterestRate = .0395;
-  } else if (score >= 790) {
-    autoInterestRate = .009;
-    homeInterestRate = .0325;
+    index=4;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $37,500.";
+  } else if (score >= 790 && score <= 850) {
+    index=5;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $75,000.";
+  } else if (score > 850) {
+    alert("A score of 850 is considered perfect.  Please choose again.");
   }
+
+  autorate = autoInterestRate[index];
 }
 
-function autoLoanAmt(score) {
-
+function homeficoscore(score, homeInterestRate) {
+  var homeindex;
   if (score < 300) {
-
+    homeindex=0;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $55,000.";
+  } else if (score >= 300 && score <= 629) {
+    homeindex=1;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $112,500.";
+  } else if (score >= 630 && score <= 689) {
+    homeindex=2;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $187,500.";
+  } else if (score >= 690 && score <= 719) {
+    homeindex=3;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $265,000.";
+  } else if (score >= 720 && score <= 789) {
+    homeindex=4;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $375,000.";
+  } else if (score >= 790 && score <= 850) {
+    homeindex=5;
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $500,000.";
+  } else if (score > 850) {
+    alert("A score of 850 is considered perfect.  Please choose again.");
   }
+  homerate = homeInterestRate[homeindex];
+}
+
+function enterAutoPrice() {
+  document.getElementById("price")
+
 }
