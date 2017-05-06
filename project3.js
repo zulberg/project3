664a2bc44f067e7f2d7d1835d31ce28beffdf10b
Unknown <zulberg@mail.csuchico.edu>
2017-05-06 14:56:06 -0700

    Continuing...

--- project3.js
+++ project3.js
@@ -12,6 +12,7 @@ var maxAutoloan=[5000, 10000, 16500, 22500, 37500, 100000];
 var autorate;
 var homerate;
 var autoMinDownPmt;
+var maxAutoPrice;
 function nextPage() {
 window.location.href="page1.html";
 }
@@ -24,33 +25,33 @@ function autoficoscore(score, autoInterestRate, maxAutoprice, maxAutoloan) {
     index=0;
     autoPriceIndex=0;
     maxAutoloanIndex=0;
-    document.getElementById("fico").innerHTML="Your maximum loan amount is " maxAutoloan+".";
-    document.getElementById("fico").innerHTML="Your maximum vehicle price is " maxAutoprice+".";
-    document.getElementById("fico").innerHTML="Your interest rate is " autoInterestRate+", or 22.5%";
+    document.getElementById("fico").innerHTML="Your maximum loan amount is $"maxAutoloan+".";
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $"maxAutoprice+".";
+    document.getElementById("fico").innerHTML="Your interest rate is "autoInterestRate+", or 22.5%";
   } else if (score >= 300 && score <= 629) {
     index=1;
     autoPriceIndex=1;
     maxAutoloanIndex=1;
     document.getElementById("fico").innerHTML="Your maximum loan amount is $10,000.";
-    document.getElementById("fico").innerHTML="Your maximum vehicle price is $27,500.";
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $22,500.";
   } else if (score >= 630 && score <= 689) {
     index=2;
     autoPriceIndex=2;
     maxAutoloanIndex=2;
     document.getElementById("fico").innerHTML="Your maximum loan amount is $16,500.";
-    document.getElementById("fico").innerHTML="Your maximum vehicle price is $50,000.";
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $35,000.";
   } else if (score >= 690 && score <= 719) {
     index=3;
     autoPriceIndex=3;
     maxAutoloanIndex=3;
     document.getElementById("fico").innerHTML="Your maximum loan amount is $22,500.";
-    document.getElementById("fico").innerHTML="Your maximum vehicle price is $70,000.";
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $50,000.";
   } else if (score >= 720 && score <= 789) {
     index=4;
     autoPriceIndex=4;
-    maxAutoloanIndex=4;
+    maxAutoloanInk8dex=4;
     document.getElementById("fico").innerHTML="Your maximum loan amount is $37,500.";
-    document.getElementById("fico").innerHTML="Your maximum vehicle price is $125,000.";
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $75,000.";
   } else if (score >= 790 && score <= 850) {
     index=5;
     maxAutoloanIndex=5;
@@ -62,6 +63,21 @@ function autoficoscore(score, autoInterestRate, maxAutoprice, maxAutoloan) {
 
   autorate = autoInterestRate[index];
 }
+
+function autoPrice(score, autoprice, maxAutoprice) {
+  autoprice=document.getElementById("aprice").value;
+  var autoindex;
+  if (score < 300 && autoprice > maxAutoprice) {
+    autoindex=0;
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $"maxAutoprice+".";
+  } else if (score >= 300 && score <= 629 && autoprice > 35000) {
+    document.getElementById("fico").innerHTML="Your maximum vehicle price is $35,000.";
+  } else if (score >= 630 && score <= 689 && autoprice >) {
+
+  }
+  maxAutoprice = maxAutoPrice[autoindex];
+}
+
 
 function homeficoscore(score, homeInterestRate) {
   var homeindex;
@@ -87,9 +103,4 @@ function homeficoscore(score, homeInterestRate) {
     alert("A score of 850 is considered perfect.  Please choose again.");
   }
   homerate = homeInterestRate[homeindex];
-}
-
-function autodownpmt() {
-  document.getElementById("price")
-
 }
